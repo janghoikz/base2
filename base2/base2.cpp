3c28c20c6cae73c0b076a6c5c5198843aefabe80
ohne <ghdlr785@gmail.com>
2022-07-19 10:11:02 +0900

    초기 커밋.

---
+++ base2/base2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//진약수의 합
+
+int main()
+{
+	int n, i, sum = 1;
+	cin >> n;
+	cout << "1";
+
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			cout << " + " << i;
+			sum = sum + i;
+		}
+	}
+	cout << " = " << sum;
+	return 0;
+}
